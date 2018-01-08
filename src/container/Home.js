23a19558b5bddd7a34c02c1352ca8f0e8c624a71
Scc <scc@SccdeMacBook-Pro.local>
2018-01-08 10:59:31 +0800

    添加无ts版本

--- src/container/Home.js
+++ src/container/Home.js
@@ -6,7 +6,7 @@ import * as actions from '../action/Home';
 @connect((states) => states
     , dispath => ({ actions: bindActionCreators(actions, dispath) }))
 
-export default class Home extends React.Component<any, any> {
+export default class Home extends React.Component{
     constructor(props) {
         super(props);
         console.log(this.props)
